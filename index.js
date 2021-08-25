76ad3a9aa495e4b3a7102d1eb40a7728ee9d2076
nonuabi <nonuabi@gmail.com>
2021-08-25 15:05:06 +0530

    create reducer

--- index.js
+++ index.js
@@ -7,3 +7,19 @@ function buyCake() {
     info: "First Redux Action",
   };
 }
+
+// reducer
+const initialState = {
+  numOfCake: 10,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        numOfCake: state - 1,
+      };
+    default:
+      return state;
+  }
+};
